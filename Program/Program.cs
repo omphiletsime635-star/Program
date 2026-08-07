d86710d82338da82701a2afe7a7fad3ae93273c9
Student <Student@LabVM2047644>
2026-08-07 08:38:36 +0200

    Add project files.

---
+++ Program/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Program;
+    using System.Media;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer player = new SoundPlayer("C:\\Users\\Student\\source\\repos\\Program\\Program\\Properties\\voice\\Rosebank College.wav");
+            player.PlaySync();
+
+            Console.WriteLine("Voice played successfully!");
+            //Console.ReadLine();
+
+            //ASCII LOGO
+
+            logo logo1 = new logo(); 
+            logo1.DisplayLogo(); 
+
+            Console.ForegroundColor = ConsoleColor.Magenta; 
+            Console.WriteLine("Welcome to the Cybersecurity AwarenessBot!");
+            Console.ResetColor();
+            Console.ReadLine(); 
+        }
+    }
+
